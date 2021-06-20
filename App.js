261d713cbbcc9e9cc19a0431025ffaeb813b80e5
dev <dev@gmail.com>
2021-06-21 00:17:52 +0530

    FIX: location support added to android

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState}from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from 'expo-location';
 
 import DateTime from './components/DateTime'
 import WeatherScroll from './components/WeatherScroll'
@@ -10,25 +11,27 @@ export default function App() {
   const [data, setData] = useState({});
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((success) => {
-        
-      let {latitude, longitude } = success.coords;
-      fetchDataFromApi(latitude,longitude )
-     
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        fetchDataFromApi("40.7128", "-74.0060")
+        return;
+      }
 
-  }, (err) => {
-    if(err){
-      fetchDataFromApi("40.7128", "-74.0060")
-    }
-  })
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
   }, [])
 
   const fetchDataFromApi = (latitude, longitude) => {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+    if(latitude && longitude) {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
-      console.log(data)
+      // console.log(data)
       setData(data)
       })
+    }
+    
   }
 
   return (
